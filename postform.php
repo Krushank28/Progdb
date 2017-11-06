fccbc38138f385005ff49b47d524d2337a66d570
Krushank28 <33370116+Krushank28@users.noreply.github.com>
2017-11-06 13:22:15 +0530

    Create postform.php

---
+++ postform.php
@@ -0,0 +1,10 @@
+<html>
+ <head><title>POST Method in Action</title></head>
+ <body>
+	<form action="post.php" method="post">
+		Name <input type="text" name="user"><br />
+		Company<input type="text" name="comp"><br />
+		<input type="submit" value=" Submit Info">
+	</form>
+ </body>
+</html>
